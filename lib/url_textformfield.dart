dfdee739cc790dcf49624c8e51017dd27f61977f
sivaprasadnk <sivaprasadnk123@gmail.com>
2022-12-06 11:15:39 +0530

    correcting documentaion

--- lib/url_textformfield.dart
+++ lib/url_textformfield.dart
@@ -3,6 +3,34 @@ library url_textformfield;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+///  Widget to make entering urls easy, without the need to type  always the leading "https://"
+///  and trailing ".com" . The widget wil show these as leading and trailing texts as th euser types.
+///
+///  [onChanged] paramater is mandatory, and is used to track the input changes
+///
+///  [focusNode] paramter is mandatory, and is used to focus the textfield by tapping
+///   anywhere in the textfield
+///
+///  [leadingText] paramter is optional. Sets the leading text.
+///
+///  [trailingText] paramter is optional. Sets the trailing text.
+///
+///  [controller] parameter is optional.
+///
+///  [textInputType] parameter is optional. Sets the input type of textfield .
+///
+///  [padding] parameter is optional. Sets the padding of the textfield.
+///
+///  [inputFormatters] paramater is optional. Sets how the input is formatted.
+///
+///  [borderRadius] paramater is optional. Sets the border radius of the textfield. Defaults to 8.
+///
+///  [borderColor] paramater is optional. Sets the border color of the textfield. Defaults to Colors.grey.
+///
+///  [height] paramater is optional. Sets the height color of the textfield. Defaults to 54.
+///
+///
+
 class UrlTextFormField extends StatelessWidget {
   const UrlTextFormField({
     required this.onChanged,
